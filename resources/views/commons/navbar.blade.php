c38bd2803735c512f52b57832d53c4e64c7ff5f8
azusaMYK <11dctat@gmail.com>
2022-05-13 09:08:11 +0000

    toppage

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,22 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        {{-- トップページ --}}
+        <a class="navbar-brand bold" href="/">Reading record</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- 読書記録登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Register Record</a></li>
+                {{-- 一覧表示へのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">List</a></li>
+                {{-- ログアウトページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Logout</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
